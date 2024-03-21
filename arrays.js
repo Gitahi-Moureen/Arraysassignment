0cd8b4b12021021d2f88d892d1349e89ed222412
Gitahi-Moureen <moureengitahiwangari@gmail.com>
2024-03-21 13:36:36 +0300

    added files

---
+++ arrays.js
@@ -0,0 +1,49 @@
+//Find the last element of the following arrays.arr1 = [3,7,34,90,12]arr2 = [true, "green", "where",12,56]
+
+let arr1 = [3,7,34,90,12];
+console.log(arr1[arr1 .length-1]);
+
+let arr2 = [true,"green","where",12,56];
+console.log(arr2[arr2.length-1]);
+
+//Write a JS program that will join the following array elements into a stringmyPets = ["Cow", "Bird", "Snake", "Dog"];
+
+let stringmyPets = ["Cow","Bird", "Snake","Dog"];
+//for...in 
+for(let a in stringmyPets){
+    console.log(stringmyPets[a]);
+}
+//to String
+console.log(stringmyPets.toString());
+
+//Write a JS script to sort the following array itemsvar arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+let arr3 = [-5,9,5,3,2,-3,6,8,4,1];
+console.log(arr3.sort());
+
+//Remove duplicates from the following array.
+var arr = ["apple","mango","apple","orange","mango","mango"];
+
+
+
+
+//Console the array without duplicate and another array that only contains duplicates
+
+
+
+
+
+//Write a JS script to search for the following word in the array."way"If the word is present, console it else console "the search word was not found"let arr5 = ["the", "way", "x", 4];
+
+
+
+
+
+
+//Write a JS script to sort the following string:let word = "sevink"
+let word = "sevink";
+
+
+
+
+
+
